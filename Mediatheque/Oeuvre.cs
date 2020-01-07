2875b28cd398d3ebb7b2a8eb89c3b179791c52df
corentin <corentin.pourchel@hotmail.fr>
2020-01-07 16:31:11 +0100

    t

--- Mediatheque/Oeuvre.cs
+++ Mediatheque/Oeuvre.cs
@@ -6,5 +6,30 @@ namespace Mediatheque
 {
     class Oeuvre
     {
+        private string titre;
+        private string auteur;
+        private string editeur;
+        private string resume;
+
+        private int nbexemplaire;
+
+
+
+        public string Titre { get => titre; set => titre = value; }
+        public string Auteur { get => auteur; set => auteur = value; }
+        public string Editeur { get => editeur; set => editeur = value; }
+        public string Resume { get => resume; set => resume = value; }
+        public int Nbexemplaire { get => nbexemplaire; set => nbexemplaire = value; }
+
+
+        public Oeuvre (string titre, string auteur, string editeur, string resume, int nbexemplaire)
+        {
+
+            Titre = titre;
+            Auteur = auteur;
+            Editeur = editeur;
+            Resume = resume;
+            Nbexemplaire = nbexemplaire;
+        }
     }
 }
