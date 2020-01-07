2ee0859d3df49baf759fc4394ee40917d8282478
florian <dessingue.florian@gmail.com>
2020-01-07 16:04:48 +0100

    Ajout constructeur adherent

--- Mediatheque/Adherent.cs
+++ Mediatheque/Adherent.cs
@@ -4,7 +4,24 @@ using System.Text;
 
 namespace Mediatheque
 {
-    class Adherent
+    public class Adherent
     {
+        protected static int compteur = 0;
+        private int id;
+        private string nom;
+        private string prenom;
+
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+
+        public Adherent(string nom, string prenom, int id)
+        {
+            compteur++;
+            Id = compteur;
+            Nom = nom;
+            Prenom = prenom;
+        }
+
     }
 }
