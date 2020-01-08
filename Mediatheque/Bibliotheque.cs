5cf74570f5eccff1c13b54e09fc4be80cc94351d
florian <dessingue.florian@gmail.com>
2020-01-08 16:54:22 +0100

    Ajout IHM

--- Mediatheque/Bibliotheque.cs
+++ Mediatheque/Bibliotheque.cs
@@ -6,7 +6,29 @@ namespace Mediatheque
 {
     public class Bibliotheque
     {
-       
+        private string nom;
+        private List<Oeuvre> oeuvres;
+        private List<Adherent> adherents;
 
+        public List<Oeuvre> Oeuvres { get => oeuvres; set => oeuvres = value; }
+        public List<Adherent> Adherents { get => adherents; set => adherents = value; }
+        public string Nom { get => nom; set => nom = value; }
+
+        public Bibliotheque(string nom)
+        {
+            Nom = nom;
+            Oeuvres = new List<Oeuvre>();
+            Adherents = new List<Adherent>();
+        }
+
+        public void AjouterOeuvre(Oeuvre o)
+        {
+            Oeuvres.Add(o);
+        }
+
+        public void AjouterAdherent(Adherent a)
+        {
+            Adherents.Add(a);
+        }
     }
 }
---
+++ Mediatheque/IHMMediatheque.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mediatheque
+{
+    public class IHMMediatheque
+    {
+        private Bibliotheque bibliotheque;
+        public void Start()
+        {
+            Console.Write("Nom hotel : ");
+            string nom = Console.ReadLine();
+            bibliotheque = new Bibliotheque(nom);
+            string choix;
+            do
+            {
+                MenuBibliotheque();
+                choix = Console.ReadLine();
+                switch (choix)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                }
+            }
+            while (choix != "0");
+        }
+
+        private void MenuBibliotheque()
+        {
+            Console.WriteLine("1---Ajouter un adhérent");
+            Console.WriteLine("2---Ajouter un client");
+            Console.WriteLine("3---Emprunter une oeuvre");
+            Console.WriteLine("4---Rendre une oeuvre");
+        }
+
+        private Oeuvre AjouterOeuvre(Oeuvre o)
+        {
+            Console.WriteLine("Quel est le nom de l'oeuvre ?");
+            string titre = Console.ReadLine();
+            Console.WriteLine("Qui est l'auteur ?");
+            string auteur = Console.ReadLine();
+            Console.WriteLine("Qui est l'editeur ?");
+            string editeur = Console.ReadLine();
+            Console.WriteLine("Quel est le resumé de l'oeuvre ?");
+            string resume = Console.ReadLine();
+            Console.WriteLine("Combien d'exemplaire ?");
+            int nbexemplaire = Convert.ToInt32(Console.ReadLine());
+            bibliotheque.AjouterOeuvre(o);
+            return o;
+        }
+
+        private Adherent AjouterAdherent(Adherent a)
+        {
+            Console.WriteLine("Quel est votre nom ?");
+            string nom = Console.ReadLine();
+            Console.WriteLine("Quel est votre prénom ?");
+            string prenom = Console.ReadLine();
+            bibliotheque.AjouterAdherent(a);
+            return a;
+        }
+
+        private void EmprunterOeuvre()
+        {
+            if (bibliotheque)
+            {
+
+            }
+        }
+
+        private void RendreOeuvre()
+        {
+
+        }
+    }
